a5ab9d581801a3fda3b6fbab240931bd8b49b7dd
lucasmirandaleite23 <lucasleite-suller@hotmail.com>
2023-01-12 14:42:13 -0300

    Função dentro do objeto

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const book = {
     bookAuthor: 'James Clear',
     bookPages: 306,
     bookChapters: {
-        chap1: 'EX',
-        chap2: 'EX2'
+        chap1: 'Fundamentals',
+        chap2: '1st law',
+    },
+    printBook: function(){
+        console.log('Printing...')
     }
 }
 
-console.log(book)
+book.printBook()
+
+
+//também podemos colocar uma função dentro do objeto, para simplificar e organizar
+//melhor o nosso código conforme mostra o exemplo acima.
+
